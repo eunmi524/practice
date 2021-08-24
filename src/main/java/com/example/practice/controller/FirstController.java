e6b64482603f4bf8b00b0b596c79af45098fa275
kim <scilo200@gmail.com>
2021-08-24 21:16:25 +0900

    기본 스프링에서 웹프로젝트로 변경 컨트롤러 추가

---
+++ src/main/java/com/example/practice/controller/FirstController.java
@@ -0,0 +1,15 @@
+package com.example.practice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class FirstController {
+
+    @GetMapping("/kim")
+    public String hello() {
+        return "hello";
+    }
+}
